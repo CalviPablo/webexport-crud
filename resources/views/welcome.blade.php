0d8667cc956a47d4db6b0d2ffd2d4f25a2080ca5
Pablo <96096465+CalviPablo@users.noreply.github.com>
2023-01-11 22:32:24 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,31 @@
 </head>
 
 <body>
-
+    <nav class="navbar navbar-expand-sm navbar-dark app-header">
+        <div class="container">
+            <Button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </Button>
+            <a href="/">
+                <img src="/images/logo-zonagamer.png" alt="Logo" />
+            </a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="/">
+                            Usuarios
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/productos">
+                            Roles
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </body>
 
 </html>
